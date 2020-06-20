002b9f2b3cd64cb5e34d5df44947ba5efdc34984
Gleb Ambrazhevich <glebikx@gmail.com>
2020-06-20 19:06:17 +0500

    Created project structure. Added dependencies

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react-scripts" />
+// Could not find @types for that libraries
+declare module 'material-icons-react';
+declare module 'enzyme-adapter-react-16';
+declare module 'enzyme';
---
+++ src/setupTests.ts
@@ -0,0 +1,8 @@
+import * as Enzyme from 'enzyme';
+import ReactSixteenAdapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom/extend-expect';
+
+Enzyme.configure({
+    adapter: new ReactSixteenAdapter(),
+    disableLifecycleMethods: true,
+});
