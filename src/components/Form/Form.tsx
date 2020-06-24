c03f5bcb1c06c0fcd6543248804fc8be769b0f26
Gleb Ambrazhevich <glebikx@gmail.com>
2020-06-24 23:56:27 +0500

    Form refactoring

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -12,10 +12,35 @@ const Form = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={style.rowWrapper}>
+          <div className={`${style.row} ${style.separator}`}>
+            <Input
+              name="firstName"
+              placeholder="First Name*"
+              ref={register({
+                required: 'This field is required',
+                pattern: { value: /^[A-Za-z]+$/i, message: 'Should contain only letters' },
+                maxLength: { value: 16, message: 'Too long' },
+              })}
+            />
+            {errors.firstName && <span className={style.err}>{errors.firstName.message}</span>}
+          </div>
+          <div className={style.row}>
+            <Input
+              name="lastName"
+              placeholder="Last Name"
+              ref={register({
+                pattern: { value: /^[A-Za-z]+$/i, message: 'Should contain only letters' },
+                maxLength: { value: 16, message: 'Too long' },
+              })}
+            />
+            {errors.lastName && <span className={style.err}>{errors.lastName.message}</span>}
+          </div>
+        </div>
         <div className={style.row}>
           <Input
             name="street"
-            placeholder="Street"
+            placeholder="Street*"
             ref={register({
               required: 'This field is required',
               minLength: { value: 6, message: 'Too short' },
@@ -23,35 +48,37 @@ const Form = () => {
           />
           {errors.street && <span className={style.err}>{errors.street.message}</span>}
         </div>
-        <div className={style.row}>
-          <Input
-            name="apt"
-            placeholder="Apartment"
-            ref={register({ required: 'This field is required' })}
-          />
-          {errors.apt && <span className={style.err}>{errors.apt.message}</span>}
+        <div className={style.rowWrapper}>
+          <div className={`${style.row} ${style.separator}`}>
+            <Input
+              name="apt"
+              placeholder="Apartment*"
+              ref={register({ required: 'This field is required' })}
+            />
+            {errors.apt && <span className={style.err}>{errors.apt.message}</span>}
+          </div>
+          <div className={`${style.row} ${style.separator}`}>
+            <Input name="porch" placeholder="Porch" ref={register} />
+            {errors.porch && <span className={style.err}>{errors.porch.message}</span>}
+          </div>
+          <div className={style.row}>
+            <Input
+              name="floor"
+              placeholder="Floor*"
+              ref={register({
+                required: 'This field is required',
+                pattern: { value: /^[0-9]+$/i, message: 'Should contain only numbers' },
+              })}
+            />
+            {errors.floor && <span className={style.err}>{errors.floor.message}</span>}
+          </div>
         </div>
         <div className={style.row}>
-          <Input
-            name="porch"
-            placeholder="Porch"
-            ref={register({ required: 'This field is required' })}
-          />
-          {errors.porch && <span className={style.err}>{errors.porch.message}</span>}
-        </div>
-        <div className={style.row}>
-          <Input
-            name="floor"
-            placeholder="Floor"
-            ref={register({ required: 'This field is required' })}
-          />
-          {errors.floor && <span className={style.err}>{errors.floor.message}</span>}
-        </div>
-        <div className={style.row}>
+          <div className={style.label}>Comment</div>
           <TextArea rows={7} name="comment" placeholder="Comment" ref={register} />
         </div>
         <div className={style.footer}>
-          {/*TODO: if basket is empty, disable button*/}
+          {/*TODO: if basket is empty, disable button. May be add info about * is required*/}
           <Button children="Confirm order" />
         </div>
       </form>
