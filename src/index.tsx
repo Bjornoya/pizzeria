4f600b483d69ae5a93be6c6717a201317d5c5da8
Gleb Ambrazhevich <glebikx@gmail.com>
2020-06-25 18:56:17 +0500

    Created Redux Store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
+import { store } from './store';
 
 ReactDOM.render(
-  <Router>
-    <Routes />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Routes />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/store.ts
@@ -0,0 +1,116 @@
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+export interface IData {
+  photo: string;
+  title: string;
+  price: string;
+  description: string;
+  id?: string;
+}
+
+interface IState {
+  data: IData[];
+  isLoading: boolean;
+  error: null | string;
+  selectedItems: Array<string>;
+}
+
+export const initialState: IState = {
+  data: [
+    {
+      photo: 'https://static.pizzasushiwok.ru/images/menu_new/6-1300.jpg',
+      title: 'Pepperoni',
+      price: '4.47€',
+      description:
+        'American variety of salami, made from a cured mixture of pork and beef seasoned ' +
+        'with paprika or other chili pepper',
+      id: 'aa',
+    },
+    {
+      photo: 'https://static.pizzasushiwok.ru/images/menu_new/6-1300.jpg',
+      title: 'Pepperoni',
+      price: '4.47€',
+      description:
+        'American variety of salami, made from a cured mixture of pork and beef seasoned ' +
+        'with paprika or other chili pepper',
+      id: 'bb',
+    },
+    {
+      photo: 'https://static.pizzasushiwok.ru/images/menu_new/6-1300.jpg',
+      title: 'Pepperoni',
+      price: '4.47€',
+      description:
+        'American variety of salami, made from a cured mixture of pork and beef seasoned ' +
+        'with paprika or other chili pepper',
+      id: 'cc',
+    },
+    {
+      photo: 'https://static.pizzasushiwok.ru/images/menu_new/6-1300.jpg',
+      title: 'Pepperoni',
+      price: '4.47€',
+      description:
+        'American variety of salami, made from a cured mixture of pork and beef seasoned ' +
+        'with paprika or other chili pepper',
+      id: 'dd',
+    },
+    {
+      photo: 'https://static.pizzasushiwok.ru/images/menu_new/6-1300.jpg',
+      title: 'Pepperoni',
+      price: '4.47€',
+      description:
+        'American variety of salami, made from a cured mixture of pork and beef seasoned ' +
+        'with paprika or other chili pepper',
+      id: 'ee',
+    },
+    {
+      photo: 'https://static.pizzasushiwok.ru/images/menu_new/6-1300.jpg',
+      title: 'Pepperoni',
+      price: '4.47€',
+      description:
+        'American variety of salami, made from a cured mixture of pork and beef seasoned ' +
+        'with paprika or other chili pepper',
+      id: 'ff',
+    },
+    {
+      photo: 'https://static.pizzasushiwok.ru/images/menu_new/6-1300.jpg',
+      title: 'Pepperoni',
+      price: '4.47€',
+      description:
+        'American variety of salami, made from a cured mixture of pork and beef seasoned ' +
+        'with paprika or other chili pepper',
+      id: 'gg',
+    },
+    {
+      photo: 'https://static.pizzasushiwok.ru/images/menu_new/6-1300.jpg',
+      title: 'Pepperoni',
+      price: '4.47€',
+      description:
+        'American variety of salami, made from a cured mixture of pork and beef seasoned ' +
+        'with paprika or other chili pepper',
+      id: 'hh',
+    },
+  ],
+  isLoading: false,
+  error: null,
+  selectedItems: [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    select: (state: IState, { payload }: PayloadAction<string>) => {
+      state.selectedItems.push(payload);
+    },
+  },
+});
+
+export const { select: selectPost } = cartSlice.actions;
+
+const reducer = {
+  cart: cartSlice.reducer,
+};
+
+export const store = configureStore({
+  reducer,
+});
